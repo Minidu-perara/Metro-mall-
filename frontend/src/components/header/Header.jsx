8b8b8faadd0db6353b60c44b7d450e7752d728df
it21308598 <it21308598@my.sliit.lk>
2024-03-16 15:57:53 +0530

    Create the admin dash board layout

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./header.css";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { height } from '@mui/system';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <div className='logo'>
+        <h1>METRO MALL</h1>
+      </div>
+      <div> 
+      <AccountCircleIcon style={{ fontSize: 40, width: 40, height: 40 }}/>
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Dashboard/DashboardLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Adminsidemenu from "../../components/AdminSidemenu/Adminsidemenu";
+import "./DashboardLayout.css";
+
+const DashboardLayout = (props) => {
+  return (
+    <div className="outsideStyle">
+      <Adminsidemenu />
+
+      <div className="innersideStyle">{props.component}</div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
