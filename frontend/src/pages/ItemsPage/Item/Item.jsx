38f9ec4b30195c06e53485f338b15c911756a9af
anupaisuru <anupaisuru2002@gmail.com>
2024-04-12 12:52:16 +0530

    done display all items in to the home page.

--- frontend/src/pages/ItemsPage/Item/Item.jsx
+++ frontend/src/pages/ItemsPage/Item/Item.jsx
@@ -3,26 +3,26 @@ import "./Item.css";
 import BurgerImg from "../../../assets/Images/items/Burger.png";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
-function Item() {
+function Item({ item }) {
   return (
     <div className="item-page">
       <div className="item-card">
         <div className="item-img">
           <img
-            src={BurgerImg}
+            src={item.imageUrls}
             alt="item-image"
             style={{ width: "260px", height: "auto" }}
           />
         </div>
         <div className="item-detail">
           <div className="item-text">
-            <h3>Supreme Chicken</h3>
-            <p>With full cream of cheese and ham</p>
-            <h5>RS 3400.00</h5>
+            <h3>{item.itemName}</h3>
+            <p>{item.description}</p>
+            <h5>RS {item.price}</h5>
           </div>
-          <div className="wishlist-icon" style={{ opacity: 0.6 }}>
+          {/* <div className="wishlist-icon" style={{ opacity: 0.6 }}>
             <FavoriteBorderIcon />
-          </div>
+          </div> */}
         </div>
         <div className="preorder-btn">
           <button>Pre Order Now</button>
--- frontend/src/pages/ItemsPage/Items/Items.jsx
+++ frontend/src/pages/ItemsPage/Items/Items.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Items.css";
 import Item from "../Item/Item";
+import axios from "axios";
 
 function Items() {
+  const [items, setItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+        const response = await axios.get("/api/item/getAllItems");
+        setItems(response.data.items);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchItems();
+  }, []);
+
+  const handleSearch = (event) => {
+    const searchQuery = event.target.value.toLowerCase();
+    const filteredItems = items.filter((item) => {
+      return item.itemName.toLowerCase().includes(searchQuery);
+    });
+    setFilteredItems(filteredItems);
+  };
+
+  const itemsToDisplay = filteredItems.length > 0 ? filteredItems : items;
+
   return (
     <div className="items-page">
       <h2>BURGER KING</h2>
       <div className="items-search">
-        <input type="text" placeholder="Search" />
+        <input
+          type="text"
+          placeholder="Search"
+          name="searchQuery"
+          onChange={handleSearch}
+        />
       </div>
       <div className="all-items">
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
+        {itemsToDisplay.map((item) => (
+          <Item item={item} key={item._id} />
+        ))}
       </div>
     </div>
   );
